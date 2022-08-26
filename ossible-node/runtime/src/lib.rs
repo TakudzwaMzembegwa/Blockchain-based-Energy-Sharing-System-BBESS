707dfb808ec39d2d85005813acbd097bde7c6194
Thandile Nododile <thandile.nododile@igoriatrade.com>
2022-08-26 22:23:18 +0200

    rm pallet

--- ossible-node/runtime/src/lib.rs
+++ ossible-node/runtime/src/lib.rs
@@ -45,9 +45,6 @@ use pallet_transaction_payment::CurrencyAdapter; // Added this import
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-/// Import the template pallet.
-pub use pallet_template;
-
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -312,11 +309,6 @@ impl pallet_sudo::Config for Runtime {
 	type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
-	type Event = Event;
-}
-
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -332,9 +324,7 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
-		// addded pallet contract to constrcut macro
+		// addded pallet contract to construct macro
 		Contracts: pallet_contracts,
 	}
 );
@@ -380,7 +370,6 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
 	);
 }
 
